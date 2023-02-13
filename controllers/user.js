57caca437b56383e03d803b2a1cbeddda0e28bc1
Alex <NaudisGarcia51@gmail.com>
2023-02-13 19:35:40 -0400

    perfil de usuario

--- controllers/user.js
+++ controllers/user.js
@@ -87,7 +87,7 @@ const register = (req, res) => {
     })
   })
 }
-
+// Login de usuarios
 const login = (req, res) => {
   // Recoger parametros body
   let params = req.body
@@ -137,10 +137,43 @@ const login = (req, res) => {
 
 
 }
+// perfil de usuario
+const profile = (req, res) => {
+  // Recibir el parametro del id de usuario por la url
+  const id = req.params.id;
+
+  // Consulta para sacar los datos del usuario
+  //const userProfile = await User.findById(id);
+
+  User.findById(id)
+      .select({ password: 0, role: 0 })
+      .exec(async (error, userProfile) => {
+          if (error || !userProfile) {
+              return res.status(404).send({
+                  status: "error",
+                  message: "El usuario no existe o hay un error"
+              });
+          }
+
+          // Info de seguimiento
+          //const followInfo = await followService.followThisUser(req.user.id, id);
+
+          // Devolver el resultado 
+          return res.status(200).send({
+              status: "success",
+              user: userProfile,
+              //following: followInfo.following,
+              //follower: followInfo.follower
+          });
+
+      });
+
+}
 
 // Exportar acciones
 module.exports = {
   pruebaUser,
   register,
   login,
+  profile,
 }
