f6708eb25f21cea953dabe7a8002b2553b9304a5
Alex <NaudisGarcia51@gmail.com>
2023-02-16 16:10:50 -0400

    save Follow

--- controllers/follow.js
+++ controllers/follow.js
@@ -1,15 +1,53 @@
-// Acciones de prueba
+// Importar modelo
+const follow = require("../models/follow")
+const Follow = require("../models/follow")
+const User = require("../models/user")
 
-const pruebafollow = (req, res) => {
+
+
+// Acciones de prueba
+const pruebaFollow = (req, res) => {
     return res.status(200).send({
         mensaje: 'Mensaje enviado desde: controller/follow.js'
     })
 }
 
+// Acción de guardar un follow (acción seguir)
+const save = (req, res) => {
+    // Conseguir datos por body
+    const params = req.body
+
+    // Sacar id del usuario identificado
+    const identity = req.user
+
+    // Crear objeto con modelo follow
+    let userToFollow = new Follow({
+        user: identity.id,
+        followed: params.followed
+    })
+
+    // Guardar objeto en bbdd
+    userToFollow.save((error, followStored) => {
+
+        if (error || !followStored) {
+            return res.status(500).send({
+                status: "error",
+                message: "No se ha podido seguir al usuario"
+            })
+        }
+
+        return res.status(200).send({
+            status: "success",
+            identity: req.user,
+            follow: followStored
+        })
+    })
+}// Fin de save
 
 
 
 // Exportar acciones
 module.exports = {
-    pruebafollow
+    pruebaFollow,
+    save
 }
--- routes/follow.js
+++ routes/follow.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const FollowController = require('../controllers/follow');
+const check = require("../middlewares/auth");
 
-// Definir las rutas 
- router.get('/prueba-Follow', FollowController.pruebafollow);
+// Definir rutas
+router.get("/prueba-follow", FollowController.pruebaFollow);
+router.post("/save", check.auth, FollowController.save);
+//router.delete("/unfollow/:id", check.auth, FollowController.unfollow);
+//router.get("/following/:id?/:page?", check.auth, FollowController.following);
+//router.get("/followers/:id?/:page?", check.auth, FollowController.followers);
 
-
- // Exportar router
+// Exportar router
 module.exports = router;
